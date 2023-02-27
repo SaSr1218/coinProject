47deb31ce6cd34e880f2902a002ecdbcdd4af10e
katonew <katonew98@gmail.com>
2023-02-27 17:52:29 +0900

    02/27 dto,dao 초기작성

--- coinProject/src/team/model/coinlist/Cdao.java
+++ coinProject/src/team/model/coinlist/Cdao.java
@@ -1,5 +1,34 @@
 package team.model.coinlist;
 
-public class Cdao {
+import java.util.ArrayList;
+import team.model.Dao;
+
+public class Cdao extends Dao{
+	// 싱글톤
+	private static Cdao cdao = new Cdao();
+	private Cdao() {}
+	public Cdao getInstance() {return cdao;}
+	
+	int coinPrice = 1500;
+	
+	//코인리스트 가져와서 담을 ArrayList
+	ArrayList<coinlistDto> coinlist = new ArrayList<>();
+	
+	// 코인 출력 메소드
+	public ArrayList<coinlistDto> print_coin() {
+		
+		return coinlist;
+	}
+	// 코인 가격변동 메소드
+	
+	public void change_coin() {
+		
+		// 코인들의 개수만큼 반복
+		for(int i=0;i<coinlist.size();i++) {
+			
+		}
+		
+	}
+	
 
 }
--- coinProject/src/team/model/coinlist/coinlistDto.java
+++ coinProject/src/team/model/coinlist/coinlistDto.java
@@ -2,4 +2,55 @@ package team.model.coinlist;
 
 public class coinlistDto {
 
+	private int cNo;
+	private String cName;
+	private int cPrice;
+	private int cAmount;
+	
+	public coinlistDto(int cNo, String cName, int cPrice, int cAmount) {
+		super();
+		this.cNo = cNo;
+		this.cName = cName;
+		this.cPrice = cPrice;
+		this.cAmount = cAmount;
+	}
+
+	public int getcNo() {
+		return cNo;
+	}
+
+	public void setcNo(int cNo) {
+		this.cNo = cNo;
+	}
+
+	public String getcName() {
+		return cName;
+	}
+
+	public void setcName(String cName) {
+		this.cName = cName;
+	}
+
+	public int getcPrice() {
+		return cPrice;
+	}
+
+	public void setcPrice(int cPrice) {
+		this.cPrice = cPrice;
+	}
+
+	public int getcAmount() {
+		return cAmount;
+	}
+
+	public void setcAmount(int cAmount) {
+		this.cAmount = cAmount;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
