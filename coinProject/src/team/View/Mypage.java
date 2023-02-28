a1e6f93afbbc987609fd51c6fc8fdb20c7b8fa59
Sengah Choi <cntjr3@naver.com>
2023-02-28 13:32:52 +0900

    2/28 13:32 기본설계

--- coinProject/src/team/View/Mypage.java
+++ coinProject/src/team/View/Mypage.java
@@ -31,18 +31,20 @@ public class Mypage {
 	} // mypage end 
 	
 	// 1.1 계좌확인
-	public void checkAccount() {
-		
+	public void checkAccount() { // if 
+		System.out.println("연동된 계좌번호는 : " ); 
+		System.out.println("출금 가능 금액은 : ");
 	}
 	
 	// 1.2 계좌입금
 	public void deposit() {
-		
+		System.out.println("입금할 금액을 입력해주세요."); int inMoney = scanner.nextInt();
 	}
 	
 	// 1.3 계좌출금
-	public void withdarw() {
-		
+	public void withdarw() { // if 출금 금액이 계좌금액보다 크다면 실패하고 "계좌에 있는 금액 이하를 입력해주세요."
+		System.out.println("출금할 금액을 입력해주세요."); int outMoney = scanner.nextInt();
+
 	}
 	
 	
--- coinProject/src/team/model/mypage/MypageDto.java
+++ coinProject/src/team/model/mypage/MypageDto.java
@@ -1,5 +1,72 @@
 package team.model.mypage;
 
 public class MypageDto {
+	// 1. 필드
+	private int accNo;		// 계좌 고유번호
+	private int Account;	// 코인 잔여개수
+	private int Balance;	// 잔고금액
+	private int mNo;		// 멤버 고유번호
+	private int cNo;		// 코인 고유번호
+	
+	// 2. 생성자
+	public MypageDto() { }
 
+	public MypageDto(int mNo, int cNo, int accNo, int account, int balance) {
+		this.mNo = mNo;
+		this.cNo = cNo;
+		this.accNo = accNo;
+		this.Account = account;
+		this.Balance = balance;
+	}
+	
+	// 3. 메소드
+	
+	@Override
+	public String toString() {
+		return "MypageDto [mNo=" + mNo + ", cNo=" + cNo + ", accNo=" + accNo + ", Account=" + Account + ", Balance="
+				+ Balance + "]";
+	}
+
+	public int getmNo() {
+		return mNo;
+	}
+
+	public void setmNo(int mNo) {
+		this.mNo = mNo;
+	}
+
+	public int getcNo() {
+		return cNo;
+	}
+
+	public void setcNo(int cNo) {
+		this.cNo = cNo;
+	}
+
+	public int getAccNo() {
+		return accNo;
+	}
+
+	public void setAccNo(int accNo) {
+		this.accNo = accNo;
+	}
+
+	public int getAccount() {
+		return Account;
+	}
+
+	public void setAccount(int account) {
+		this.Account = account;
+	}
+
+	public int getBalance() {
+		return Balance;
+	}
+
+	public void setBalance(int balance) {
+		this.Balance = balance;
+	}
+	
+	
+	
 }
