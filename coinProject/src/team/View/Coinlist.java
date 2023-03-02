91ff4b38f2f7ff386da3b67b30491225df58c9ef
katonew <katonew98@gmail.com>
2023-03-02 16:28:58 +0900

    03/02 16:28 코인출력 멀티스레드

--- coinProject/src/team/View/Coinlist.java
+++ coinProject/src/team/View/Coinlist.java
@@ -5,28 +5,51 @@ import java.util.ArrayList;
 import team.controller.Ccontroller;
 import team.model.coinlist.coinlistDto;
 
-public class Coinlist {
+public class Coinlist extends Thread {
 	
 	//싱글톤
 	private static Coinlist coinlist = new Coinlist();
 	private Coinlist() {}
 	public static Coinlist getInstance() {return coinlist;}
 	
+	boolean stop = false;
 	
-	public void print_coin() {
-		while(true) {
-			try {Thread.sleep(1000); }	// 5초에 한번씩 업데이트
-			catch (Exception e) {}
+	public boolean isStop() {
+		return stop;
+	}
+	public void setStop(boolean stop) {
+		this.stop = stop;
+	}
+	@Override
+	public void run() {
+		while(stop) {
 			ArrayList<coinlistDto> result = Ccontroller.getInstance().print_coin();
 			System.out.println("---------------------코인리스트----------------------");
 			System.out.println("번호\t코인이름\t가격\t기초대비");
 			for(int i=0;i<result.size();i++) {
 				System.out.printf("%d\t%s\t%d\t%.2f\n",
-						result.get(i).getcNo(),result.get(i).getcName(),result.get(i).getcPrice(),
-						((double)result.get(i).getcPrice()/(double)result.get(i).getcFirstPrice())*100
-						);
+				result.get(i).getcNo(),result.get(i).getcName(),result.get(i).getcPrice(),
+				((double)result.get(i).getcPrice()/(double)result.get(i).getcFirstPrice())*100-100
+				);
 			}
+			System.out.println("1. 매수 / 2. 매도 / 3. 손익확인(임시)");
+			try {Thread.sleep(5000); }	// 5초에 한번씩 업데이트
+			catch (Exception e) {}
+			
+		} // while e
+	} // run e
+	
+	public void print_coin() {		
+		try {Thread.sleep(1000); }	// 5초에 한번씩 업데이트
+		catch (Exception e) {}
+		ArrayList<coinlistDto> result = Ccontroller.getInstance().print_coin();
+		System.out.println("---------------------코인리스트----------------------");
+		System.out.println("번호\t코인이름\t가격\t기초대비");
+		for(int i=0;i<result.size();i++) {
+			System.out.printf("%d\t%s\t%d\t%.2f\n",
+					result.get(i).getcNo(),result.get(i).getcName(),result.get(i).getcPrice(),
+					((double)result.get(i).getcPrice()/(double)result.get(i).getcFirstPrice())*100-100
+					);
 		}
-		
 	}// print_coin e
 } // class e
