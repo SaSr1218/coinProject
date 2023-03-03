d9f50baf6f89601d3fb0a712492c9ae48348bf14
504 <504@DESKTOP-TVIPHFD>
2023-03-03 17:29:06 +0900

    2023-03-03 17:29

--- coinProject/src/2023-03-02 수정.sql
+++ coinProject/src/2023-03-02 수정.sql
@@ -91,7 +91,8 @@ create table coinTradeList(
     CTPrice int not null,
     CTVolume int not null,
     CTDate datetime default now(),
-    TradeState char(1) not null,
+    Buystate char(1),
+    Sellstate char(1) default null,
     cNo int,
     mNo int,
     foreign key ( cNo ) references coinlist ( cNo ) on delete cascade,
@@ -105,44 +106,82 @@ insert into coinlist ( cName , cPrice , cAmount ) value ('비트코인' , 1000 ,
 insert into coinmarketp ( ciprice , cmprice , cmremaining , cno ) values ( 1500 , 1500 , 100 , 1);
 
 	-- 일반사용자 코인 구매 쿼리 ( 2번 유저가 1번 코인 1500원에 20개 구매 )
-insert into cointradelist ( ctprice , ctvolume , tradestate , cno , mno ) values ( 1500 , 20 , 'b' , 1 , 2) ;
+insert into cointradelist ( ctprice , ctvolume , Buystate , cno , mno ) values ( 1500 , 20 , 'b' , 1 , 2) ;
 update coinmarketp p , cointradelist t set p.cmremaining = (p.cmremaining - 20) where p.cmno = 1;
 
 	-- 일반사용자 코인 구매 쿼리 ( 3번 유저가 1번코인 1300원에 50개 구매 ) 
-insert into cointradelist ( ctprice , ctvolume , tradestate , cno , mno ) values ( 1300 , 50 , 'b' , 1 , 3) ;
+insert into cointradelist ( ctprice , ctvolume , Buystate , cno , mno ) values ( 1300 , 50 , 'b' , 1 , 3) ;
 update coinmarketp p , cointradelist t set p.cmremaining = (p.cmremaining - 50) where p.cmno = 1;
 
-	-- 일반사용자 코인 판매 쿼리 ( 2번 유저가 1번코인 1400원에 40개 판매 )
-insert into cointradelist ( ctprice , ctvolume , tradestate , cno , mno ) values ( 1400 , 40 , 's' , 1 , 3) ;
-update coinmarketp p , cointradelist t set p.cmremaining = (p.cmremaining + 30) where p.cmno = 1;
+	-- 일반사용자 코인 판매 쿼리 ( 3번 유저가 1번코인 1400원에 40개 판매 )
+insert into cointradelist ( ctprice , ctvolume , Buystate, Sellstate , cno , mno ) values ( 1400 , 40 , 'b' , 's' , 1 , 3) ;
+update coinmarketp p , cointradelist t set p.cmremaining = (p.cmremaining + 40) where p.cmno = 1;
 
-select (p.ciprice + sum(t.ctprice))/p.cmremaining , ( p.cmremaining + t.ctvolume ) 
-from coinmarketp p , cointradelist t 
-where p.cno = t.cno and t.tradestate = 's' 
-group by t.ctprice , t.ctvolume , p.ciprice , p.cmremaining ;
+insert into cointradelist ( ctprice , ctvolume , Buystate, Sellstate , cno , mno ) values ( 1700 , 50 , 'b' , 's' , 1 , 3) ;
+update coinmarketp p , cointradelist t set p.cmremaining = (p.cmremaining + 50) where p.cmno = 1;
 
--- 평단가 구하는 쿼리
-select sum((t.ctprice*t.ctvolume) + (p.ciprice*p.cmremaining))/c.camount as result
+
+select (t.ctprice*t.ctvolume) + (p.ciprice*p.cmremaining) as result
 from cointradelist t , coinlist c , coinmarketp p 
-where c.cno = t.cno and c.cno = 1 and t.tradestate = 's';
-
-update coinmarketp p , 
-(select sum((t.ctprice*t.ctvolume) + (p.ciprice*p.cmremaining))/c.camount as result
-from cointradelist t , coinlist c , coinmarketp p 
-where c.cno = t.cno and c.cno = 1 and t.tradestate = 's') r
-set p.CMprice = r.result where p.cno = 1;
+where c.cno = t.cno and c.cno = 1 and sellstate = 's' ;
 
 
-select * from coinmarketp ;
+-- ( CIprice( 초기단가) * CMRemaining( 남은코인갯수) ) + ( ctprice(최근판매단가)  * ctvolume(구매수량) )
 
-select * from member;
-select * from coinlist;
-select * from buy;
-select * from sell;
-select * from account ;
-select * from create_acc;
+-- 최근판매단가 구하는 쿼리
+select ctprice from cointradelist where sellstate is not null order by ctdate desc limit 1 ;
+
+-- 구매수량 구하는 쿼리
+select 
+((select sum(ctvolume) from cointradelist where cno = 1 and sellstate is null)
+ - (select sum(ctvolume) from cointradelist where cno = 1 and sellstate is not null))
+from (select sum(ctvolume) from cointradelist where cno = 1 and sellstate is null) buycoin , 
+(select sum(ctvolume) from cointradelist where cno = 1 and sellstate is not null) sellcoin;
+
+-- 최근거래단가 * 구매된수량
+select
+	(select ctprice from cointradelist where sellstate is not null order by ctdate desc limit 1)
+    *
+    (select 
+	((select sum(ctvolume) from cointradelist where cno = 1 and sellstate is null)
+	 - (select sum(ctvolume) from cointradelist where cno = 1 and sellstate is not null))
+	from (select sum(ctvolume) from cointradelist where cno = 1 and sellstate is null) buycoin , 
+	(select sum(ctvolume) from cointradelist where cno = 1 and sellstate is not null) sellcoin)
+from dual;
+
+-- (( CIprice( 초기단가) * CMRemaining( 남은코인갯수) ) + ( ctprice(최근판매단가)  * ctvolume(구매수량) )) / 전체갯수 최종쿼리
+select ((p.ciprice*p.cmremaining) +
+	(select
+		(select ctprice from cointradelist where sellstate is not null order by ctdate desc limit 1)
+		*
+		(select 
+		((select sum(ctvolume) from cointradelist where cno = 1 and sellstate is null)
+		 - (select sum(ctvolume) from cointradelist where cno = 1 and sellstate is not null))
+		from (select sum(ctvolume) from cointradelist where cno = 1 and sellstate is null) buycoin , 
+		(select sum(ctvolume) from cointradelist where cno = 1 and sellstate is not null) sellcoin)
+	from dual)) / c.cAmount as result
+from coinmarketp p , coinlist c where p.cno = 1 ;
+
+-- 현단가로 업데이트 쿼리
+update coinmarketp p,
+	(select ((p.ciprice*p.cmremaining) +
+		(select
+			(select ctprice from cointradelist where sellstate is not null order by ctdate desc limit 1)
+			*
+			(select 
+			((select sum(ctvolume) from cointradelist where cno = 1 and sellstate is null)
+			 - (select sum(ctvolume) from cointradelist where cno = 1 and sellstate is not null))
+			from (select sum(ctvolume) from cointradelist where cno = 1 and sellstate is null) buycoin , 
+			(select sum(ctvolume) from cointradelist where cno = 1 and sellstate is not null) sellcoin)
+		from dual)) / c.cAmount as result
+	from coinmarketp p , coinlist c where p.cno = 1) r
+set p.cmprice = r.result where p.cno = 1;
+
+
+
+
+
+
+
 select * from coinmarketp;
 select * from cointradelist ;
-
-
-
